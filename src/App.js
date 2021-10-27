cbe0355877be7649e9d59423e929dc57f12ec0e7
Jv Penha <joaovictorpenhaalves@gmail.com>
2021-10-27 02:53:49 -0300

    chamando o componente principal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderResponsive from './Componts/HeaderResponsive';
+import MainSection from './Componts/MainSection';
 
 function App() {
   return (
     <div className="App">
+      <HeaderResponsive />
+      <MainSection />
 
     </div>
   );
