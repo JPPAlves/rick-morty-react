8dfe13f0e5eae9aaf3bc14750ddefa43eee4db7d
Jv Penha <joaovictorpenhaalves@gmail.com>
2021-11-01 17:30:17 -0300

    Add pasta da páginas

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import HeaderResponsive from "../../Componts/HeaderResponsive";
+import MainSection from "../../Componts/MainSection";
+
+function Home() {
+    return (
+        <div>
+            <HeaderResponsive />
+            <MainSection />
+        </div>
+
+    );
+}
+
+export default Home;
