a29fb83735d9f866a41b08029935163e7a53ee53
Jv Penha <joaovictorpenhaalves@gmail.com>
2021-11-01 22:16:36 -0300

    Criando o search-container

--- src/Pages/Personagens/index.js
+++ src/Pages/Personagens/index.js
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import HeaderResponsive from "../../Componts/HeaderResponsive";
+import api from "../../Api";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
 function Personagens() {
+
+    const [data, setData] = useState(null);
+    const [name, setName] = useState("");
+    useEffect(() => {
+        // async function loadAll() {
+        //     let res = await api.getPersonagemByName();
+        //     setData(res.data)
+        //     console.log(res.data)
+
+        // }
+        // loadAll();
+    }, []);
+
+    function ClickMouse(event) {
+        event.preventDefault();
+        console.log("Escolhendo nome:", name);
+        //let res = await api.getPersonagemByName(name);
+    }
+
+
     return (
 
-        <div>Hello world</div>
+        <div>
+            <HeaderResponsive />
+            <div className="search-container">
+                <form onSubmit={ClickMouse}>
+                    <input onChange={(e) => {
+                        setName(e.target.value);
+                    }} />
+                    <button type="submit">
+                        <FontAwesomeIcon icon={faSearch} size="lg" />
+                    </button>
+
+
+                </form>
+
+            </div>
+        </div>
 
     );
 }
