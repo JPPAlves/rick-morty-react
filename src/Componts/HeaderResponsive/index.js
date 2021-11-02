d69f02b7aaf81b82f7ef6459069e3d0e15ec01fb
Jv Penha <joaovictorpenhaalves@gmail.com>
2021-11-01 22:19:54 -0300

    Criando o evento openMenu

--- src/Componts/HeaderResponsive/index.js
+++ src/Componts/HeaderResponsive/index.js
@@ -20,9 +20,9 @@ function HeaderResponsive() {
             </div>
             <ul className={`menu-options${active ? ` active` : ' '}`}>
                 <li><a href="/">Home</a></li>
-                <li><a href="/">Personagens</a></li>
+                <li><a href="/Personagens">Personagens</a></li>
                 <li><a href="/">Episódios</a></li>
-                <li><a href="/">Sobre</a></li>
+                <li><a href="/Sobre">Sobre</a></li>
 
 
             </ul>
