3fc5aa1778588c4a7dccee660f0296ce59c7ec88
Jv Penha <joaovictorpenhaalves@gmail.com>
2021-11-08 19:22:32 -0300

    Add pagina dos personagem

--- src/Pages/Personagem/index.js
+++ src/Pages/Personagem/index.js
@@ -1,8 +1,66 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../Api";
+import "./styles.css";
+import HeaderResponsive from "../../Componts/HeaderResponsive";
+
 function Personagem() {
+    const { id } = useParams();
+    const [data, setData] = useState(null);
+    const [dataCreated, setdataCreated] = useState(null);
+
+    useEffect(() => {
+        const load = async () => {
+            let response = await api.getPersonagemById(id);
+            setData(response);
+
+            let data_criacao = new Date(response.created)
+            data_criacao = data_criacao.toLocaleDateString("pt-BR");
+
+            setdataCreated(data_criacao);
+
+        }
+        load();
+    }, [id])
+
     return (
+        <>
+            <HeaderResponsive />
+            <div className="personagem-container">
+                {data &&
+                    (
+                        <div className="full-info-data-container">
+                            <img src={data.image} alt={data.name} />
+                            <div className="info-data-container">
+                                <h1>Informações Completas</h1>
+                                <p>Nome:</p>
+                                <span>{data.name}</span>
 
-        <div>Hello Personagem</div>
+                                <p>Estado:</p>
+                                <span>{data.status}</span>
 
+                                <p>Espécie:</p>
+                                <span>{data.species}</span>
+
+                                <p>Gênero:</p>
+                                <span>{data.gender}</span>
+
+                                <p>Visto pela primeira em:</p>
+                                <span>{data.origin.name}</span>
+
+                                <p>Visto pela última vez em:</p>
+                                <span>{data.location.name}</span>
+
+                                <p>Criado em:</p>
+                                <span>{dataCreated}</span>
+
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        </>
     );
 }
+
 export default Personagem;
